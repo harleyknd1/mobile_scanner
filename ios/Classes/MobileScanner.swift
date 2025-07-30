1ef122c584861b2a9a22cd42fc25f4dcf5169bf5
HarleyLafeber <harley@sentinel-it.nl>
2025-07-30 14:02:22 +0200

    * remove deprecated function that's preventing build

--- ios/Classes/MobileScanner.swift
+++ ios/Classes/MobileScanner.swift
@@ -246,7 +246,7 @@ public class MobileScanner: NSObject, AVCaptureVideoDataOutputSampleBufferDelega
 
         captureSession!.addOutput(videoOutput)
         for connection in videoOutput.connections {
-            connection.videoOrientation = .portrait
+            connection.videoRotationAngle = .portrait
             if cameraPosition == .front && connection.isVideoMirroringSupported {
                 connection.isVideoMirrored = true
             }
